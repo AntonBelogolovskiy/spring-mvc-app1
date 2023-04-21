fe22eb5fa5c8cc8bbe41d9eda1ab295549c79306
Anton <hector123@bk.ru>
2023-04-21 22:27:52 +0300

    21.04.2023

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,6 +1,8 @@
 
-7
-pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+p
+@src/main/java/ru/hector/spring/controllers/SecondController.java,5\1\5143bd0fe163e9f4487b7b36dbd9fb4206034f2a
+^
+.src/main/webapp/WEB-INF/views/second/exit.html,8\6\86c85c7f48399a46de70bd7f0021446be98f0b76
--- src/main/java/ru/hector/spring/controllers/FirstController.java
+++ src/main/java/ru/hector/spring/controllers/FirstController.java
@@ -1,15 +1,50 @@
 package ru.hector.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name",required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//        System.out.println("hello, " + name + " " + surname);
+
+        model.addAttribute("message", "hello, " + name + " " + surname);
+
+
         return "first/hello";
+    }
+
+    @GetMapping("/calculator")
+    public String calc(@RequestParam(value = "arg1",defaultValue = "1") int arg1,
+                       @RequestParam(value = "arg2",defaultValue = "1") int arg2,
+                       @RequestParam(value = "action",defaultValue = "addition") String action,
+                       Model model) {
+        double result = 0;
+        switch (action) {
+            case "multiplication":
+                result = arg1 * arg2;
+                break;
+            case "addition":
+                result = arg1 + arg2;
+                break;
+            case "subtraction":
+                result = arg1 - arg2;
+                break;
+            case "division":
+                result = ((double) arg1) / arg2;
+                break;
+        }
+        model.addAttribute("result", result);
+        return "first/calculator";
     }
 
     @GetMapping("/goodbye")
